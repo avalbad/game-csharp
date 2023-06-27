d6597254b445a70493aa43cfc3b4d0e5a2c46263
avalbad <131601395+avalbad@users.noreply.github.com>
2023-06-27 11:35:34 +0330

    Add files via upload

---
+++ c#-game/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl00 = new System.Windows.Forms.Label();
+            this.lbl02 = new System.Windows.Forms.Label();
+            this.lbl01 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl21 = new System.Windows.Forms.Label();
+            this.lbl22 = new System.Windows.Forms.Label();
+            this.lbl20 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl00
+            // 
+            this.lbl00.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl00.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl00.Location = new System.Drawing.Point(49, 43);
+            this.lbl00.Name = "lbl00";
+            this.lbl00.Size = new System.Drawing.Size(55, 40);
+            this.lbl00.TabIndex = 0;
+            this.lbl00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl00.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl02
+            // 
+            this.lbl02.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl02.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl02.Location = new System.Drawing.Point(167, 43);
+            this.lbl02.Name = "lbl02";
+            this.lbl02.Size = new System.Drawing.Size(55, 40);
+            this.lbl02.TabIndex = 1;
+            this.lbl02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl02.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl01
+            // 
+            this.lbl01.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl01.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl01.Location = new System.Drawing.Point(108, 43);
+            this.lbl01.Name = "lbl01";
+            this.lbl01.Size = new System.Drawing.Size(55, 40);
+            this.lbl01.TabIndex = 2;
+            this.lbl01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl01.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl11
+            // 
+            this.lbl11.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl11.Location = new System.Drawing.Point(108, 90);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(55, 40);
+            this.lbl11.TabIndex = 5;
+            this.lbl11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl11.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl12
+            // 
+            this.lbl12.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl12.Location = new System.Drawing.Point(167, 90);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(55, 40);
+            this.lbl12.TabIndex = 4;
+            this.lbl12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl12.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl10
+            // 
+            this.lbl10.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl10.Location = new System.Drawing.Point(49, 90);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(55, 40);
+            this.lbl10.TabIndex = 3;
+            this.lbl10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl10.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl21
+            // 
+            this.lbl21.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl21.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl21.Location = new System.Drawing.Point(108, 137);
+            this.lbl21.Name = "lbl21";
+            this.lbl21.Size = new System.Drawing.Size(55, 40);
+            this.lbl21.TabIndex = 8;
+            this.lbl21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl21.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl22
+            // 
+            this.lbl22.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl22.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl22.Location = new System.Drawing.Point(167, 137);
+            this.lbl22.Name = "lbl22";
+            this.lbl22.Size = new System.Drawing.Size(55, 40);
+            this.lbl22.TabIndex = 7;
+            this.lbl22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl22.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // lbl20
+            // 
+            this.lbl20.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lbl20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl20.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl20.Location = new System.Drawing.Point(49, 137);
+            this.lbl20.Name = "lbl20";
+            this.lbl20.Size = new System.Drawing.Size(55, 40);
+            this.lbl20.TabIndex = 6;
+            this.lbl20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl20.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label10.Location = new System.Drawing.Point(34, 203);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(197, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "بر روی هر خانه کلیک چپ یا راست کنید";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(282, 242);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbl21);
+            this.Controls.Add(this.lbl22);
+            this.Controls.Add(this.lbl20);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl12);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl01);
+            this.Controls.Add(this.lbl02);
+            this.Controls.Add(this.lbl00);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "بازی دوز";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl00;
+        private System.Windows.Forms.Label lbl02;
+        private System.Windows.Forms.Label lbl01;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lbl12;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl21;
+        private System.Windows.Forms.Label lbl22;
+        private System.Windows.Forms.Label lbl20;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ c#-game/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int[,] xo = new int[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Mouse(object sender, MouseEventArgs e)
+        {
+            int i,j;
+            i =int.Parse(((Label)sender).Name[3].ToString());
+            j =int.Parse(((Label)sender).Name[4].ToString());
+
+            if (e.Button == MouseButtons.Left)
+            {
+                ((Label)sender).Text = "1";
+                xo[i, j] = 1;
+                check(i,
+                    j,
+                    1);
+            }
+
+            else
+            {
+                ((Label)sender).Text = "2";
+                xo[i, j] = 2;
+                check(i ,j ,2);
+            }     
+        }
+
+
+        public void check(int i, int j, int person)
+        {
+            rowcheck(i, j, person);
+            colcheck(i, j, person);
+            gotraslicheck(person);
+            gotrafarecheck(person);
+        }
+
+        public void rowcheck(int i, int j, int person)
+        {
+
+            if (xo[i,0] == person && xo[i,1] == person && xo[i,2]== person) 
+            {
+                MessageBox.Show("بازیکن شماره "+person.ToString()+" برنده شد");
+            }
+        }
+
+
+        public void colcheck(int i, int j, int person)
+        {
+
+            if (xo[0,j] == person && xo[1,j] == person && xo[2,j] == person)
+            {
+                MessageBox.Show("بازیکن شماره " + person.ToString() + " برنده شد");
+            }
+        }
+        public void gotraslicheck(int person)
+        {
+
+            if (xo[0,0] == person && xo[1,1] == person && xo[2,2] == person)
+            {
+                MessageBox.Show("بازیکن شماره " + person.ToString() + " برنده شد");
+            }
+        }
+
+        public void gotrafarecheck(int person)
+        {
+
+            if (xo[2,0] == person && xo[1,1] == person && xo[0,2] == person)
+            {
+                MessageBox.Show("بازیکن شماره " + person.ToString() + " برنده شد");
+            }
+        }
+
+    }
+}
